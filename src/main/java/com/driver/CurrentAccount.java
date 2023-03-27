fb1afc10ae287fe3b84481b58fb3afeb58633185
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-03-27 19:55:40 +0530

    Initial Commit

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,9 +3,21 @@ package com.driver;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
+
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
 
+        if (balance < 5000)
+            throw new Exception("Insufficient Balance");
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +25,83 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        if (!isValid(tradeLicenseId))
+        {
+            String reArrange = change(tradeLicenseId);
 
+            if(reArrange == "")
+                throw new Exception("Valid License can not be generated");
+            else
+                this.tradeLicenseId = reArrange;
+        }
     }
 
+    public String change(String tradeLicenseId)
+    {
+        int n = tradeLicenseId.length();
+
+        int freq[] = new int[26];
+
+        for (int i = 0; i < n; i++)
+            freq[tradeLicenseId.charAt(i) - 'A']++;
+
+        int max = 0, letter = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (max < freq[i]) {
+                max = freq[i];
+                letter = i;
+            }
+        }
+
+        if (max > (n + 1) / 2)
+            return "";
+
+        char ch[] = new char[n];
+
+        // For Even position filling
+        for (int i = 0; i < n; i += 2) {
+            if (freq[letter] > 0) {
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    if (freq[j] > 0) {
+                        letter = j;
+                        break;
+                    }
+                }
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+        }
+
+        // For Odd position filling
+        for (int i = 1; i < n; i += 2) {
+            if (freq[letter] > 0) {
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    if (freq[j] > 0) {
+                        letter = j;
+                        break;
+                    }
+                }
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+        }
+        return String.valueOf(ch);
+    }
+
+    public boolean isValid(String tradeLicenseId)
+    {
+        for(int i=0;i<tradeLicenseId.length()-1;i++)
+        {
+            if(tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i+1))
+                return false;
+        }
+        return true;
+    }
 }
